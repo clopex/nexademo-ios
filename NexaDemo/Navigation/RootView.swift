1fea0377038cca7848ee4b3afc384d4cf93b7703
Adis Mulabdic <adis.mulabdic@gmail.com>
2026-02-25 11:20:12 +0100

    refactor: inject authVM/sheet via environment, move RootView

---
+++ NexaDemo/Navigation/RootView.swift
@@ -0,0 +1,30 @@
+import SwiftUI
+
+struct RootView: View {
+    @Environment(AuthViewModel.self) private var authVM
+    @Environment(AppSheetManager.self) private var sheetManager
+
+    var body: some View {
+        Group {
+            if authVM.isLoggedIn {
+                MainTabView()
+            } else {
+                AuthFlowView()
+            }
+        }
+        .sheet(item: $sheetManager.activeSheet) { sheet in
+            switch sheet {
+            case .editProfile: EditProfileView()
+            case .paywall: PaywallView()
+            case .imagePicker: ImagePickerView()
+            }
+        }
+        .fullScreenCover(item: $sheetManager.activeFullScreen) { screen in
+            switch screen {
+            case .camera: CameraView()
+            case .onboarding: OnboardingView()
+            case .videoCall(let channel): VideoCallView(channel: channel)
+            }
+        }
+    }
+}
--- NexaDemo/NexaDemoApp.swift
+++ NexaDemo/NexaDemoApp.swift
@@ -2,9 +2,14 @@ import SwiftUI
 
 @main
 struct NexaDemoApp: App {
+    @State private var authVM = AuthViewModel()
+    @State private var sheetManager = AppSheetManager()
+
     var body: some Scene {
         WindowGroup {
             RootView()
+                .environment(authVM)
+                .environment(sheetManager)
         }
     }
 }
